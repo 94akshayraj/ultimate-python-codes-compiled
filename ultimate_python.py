f47049d9d2332c78f387f9ce1dc28c567d596e51
Akshay. R <akhisreelibra@gmail.com>
2019-11-11 14:23:43 +0530

    Update ultimate_python.py

--- ultimate_python.py
+++ ultimate_python.py
@@ -1,8 +1,12 @@
 # THE ULTIMATE PYTHON COMPILE
 
 # CHECK PYTHON DIR version
-
 import sys
 print(sys.executable)
 print(sys.version)
 print(sys.version_info)
+
+# BAG OF WORDS (BOW) aka COUNT VECTORIZER
+vectorizer = CountVectorizer()
+print( vectorizer.fit_transform(corpus).todense() )
+print( vectorizer.vocabulary_ )
